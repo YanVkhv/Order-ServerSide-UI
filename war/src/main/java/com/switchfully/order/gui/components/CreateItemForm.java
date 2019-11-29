ad7d1ece4f4cc50ff5cd519599ef487079e46f48
YanVkhv <yan.vkhv@gmail.com>
2019-11-29 14:32:23 +0100

    Added create and cancel buttons

--- war/src/main/java/com/switchfully/order/gui/components/CreateItemForm.java
+++ war/src/main/java/com/switchfully/order/gui/components/CreateItemForm.java
@@ -2,12 +2,14 @@ package com.switchfully.order.gui.components;
 
 import com.switchfully.order.api.items.ItemOverviewDto;
 import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.provider.ListDataProvider;
 
 public class CreateItemForm extends Composite<FormLayout> {
@@ -18,18 +20,28 @@ public class CreateItemForm extends Composite<FormLayout> {
     private final NumberField price = new NumberField();
     private final NumberField stockAmount = new NumberField();
     private final HorizontalLayout priceAndStock = new HorizontalLayout();
+    private final HorizontalLayout buttons = new HorizontalLayout();
     private final VerticalLayout completeForm = new VerticalLayout();
+    private final Binder<ItemOverviewDto> binder = new Binder<>(ItemOverviewDto.class);
+    private final Button cancel = new Button("Cancel");
+    private final Button create = new Button("Create");
 
     public CreateItemForm(ListDataProvider<ItemOverviewDto> itemDataProvider) {
         this.itemDataProvider = itemDataProvider;
+
+        binder.forField(name)
+                .bind(ItemOverviewDto::getName, ItemOverviewDto::withName);
 
         name.setLabel("Name");
         description.setLabel("Description");
         price.setLabel("Price");
         stockAmount.setLabel("Amount in Stock");
         priceAndStock.add(price, stockAmount);
-        completeForm.add(name, description, priceAndStock);
+        buttons.add(create, cancel);
+        completeForm.add(name, description, priceAndStock, buttons);
 
+        create.setWidth("660px");
+        cancel.setWidth("110px");
         description.setMaxLength(255);
         description.setPlaceholder("Max. length: " + description.getMaxLength() + " characters");
         description.setHeight("200px");
@@ -38,4 +50,5 @@ public class CreateItemForm extends Composite<FormLayout> {
 
         getContent().add(completeForm);
     }
+
 }
